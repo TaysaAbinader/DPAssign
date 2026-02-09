931ca9d79a8d89bfb9db6e0eb65c82304f7fd878
Taysa Abinader <ferreira.taysa@gmail.com>
2026-02-09 11:03:56 +0200

    DEcorator Assignment

---
+++ Decorator/EncryptedPrinter.java
@@ -0,0 +1,15 @@
+package Decorator;
+
+import java.util.Base64;
+
+public class EncryptedPrinter extends PrinterDecorator{
+    public EncryptedPrinter(Printer printer) {
+        super(printer);
+    }
+
+    @Override
+    public void print(String message) {
+        String encrypted = Base64.getEncoder().encodeToString(message.getBytes());
+        super.print(encrypted);
+    }
+}
---
+++ Decorator/PrinterDecorator.java
@@ -0,0 +1,13 @@
+package Decorator;
+
+public abstract class PrinterDecorator implements Printer{
+    protected Printer decoratedPrinter;
+
+    public PrinterDecorator(Printer printer) {
+        this.decoratedPrinter = printer;
+    }
+
+    public void print(String message) {
+        decoratedPrinter.print(message);
+    }
+}
